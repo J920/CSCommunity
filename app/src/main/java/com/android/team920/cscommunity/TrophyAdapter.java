2f5340d540e781bce03c740b9014e2600190f781
MohaZain <x7mood07@gmail.com>
2018-10-04 19:48:24 +0300

    question page
    سيرش في صفحه الاسئله

---
+++ app/src/main/java/com/android/team920/cscommunity/TrophyAdapter.java
@@ -0,0 +1,45 @@
+package com.android.team920.cscommunity;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TrophyAdapter extends FragmentStatePagerAdapter {
+    int mNoOfTabs;
+    public TrophyAdapter(FragmentManager fm, int NumberOfTabs) {
+        super(fm);
+        this.mNoOfTabs = NumberOfTabs;
+
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position)
+        {
+
+            case 0:
+                AnswerFragment tap1 = new AnswerFragment();
+                return  tap1;
+
+            case 1:
+                YourQuestionsFragment tap2 = new YourQuestionsFragment();
+                return tap2;
+
+            case 2:
+                YourLikeQuesitonsFragment tap3 = new YourLikeQuesitonsFragment();
+                return tap3;
+
+            default:
+                return null;
+        }
+    }
+
+
+    @Override
+    public int getCount() {
+        return mNoOfTabs;
+    }
+}
+
+
